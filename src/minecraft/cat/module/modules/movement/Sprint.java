63e5776d661f3b0ff6ef4d0b96127892db9c9b5e
hahayesfun <sutku.turkyilmaz@gmail.com>
2021-07-15 22:52:20 +0300

    sprint fix

--- src/minecraft/cat/module/modules/movement/Sprint.java
+++ src/minecraft/cat/module/modules/movement/Sprint.java
@@ -1,29 +1,35 @@
 package cat.module.modules.movement;
 
-        import cat.events.Subscriber;
-        import cat.events.impl.UpdateEvent;
-        import cat.module.Module;
-        import cat.module.ModuleCategory;
-        import cat.util.MovementUtil;
+import cat.events.Subscriber;
+import cat.events.impl.UpdateEvent;
+import cat.module.Module;
+import cat.module.ModuleCategory;
+import cat.module.value.types.BoolValue;
+import cat.module.value.types.ModeValue;
+import cat.util.MovementUtil;
 
-        @SuppressWarnings("unused")
-        public class Sprint extends Module {
-        public Sprint() {
+@SuppressWarnings("unused")
+public class Sprint extends Module {
+    public Sprint() {
         super("Sprint", "", ModuleCategory.MOVEMENT, "Sprint");
         this.setState(true);
-        }
+    }
 
-        @Subscriber
-        public void onUpdate(UpdateEvent e) {
+    @Subscriber
+    public void onUpdate(UpdateEvent e) {
         if (MovementUtil.areMovementKeysPressed()) {
-        if (!mc.thePlayer.isCollidedHorizontally) {
-        if (!mc.thePlayer.isUsingItem()) {
-        if (!mc.thePlayer.isSneaking()) {
-        mc.thePlayer.setSprinting(true);
+            if (!mc.thePlayer.isCollidedHorizontally) {
+                if (!mc.thePlayer.isUsingItem()) {
+                    if (!mc.thePlayer.isSneaking()) {
+                        mc.thePlayer.setSprinting(true);
+                    }
+                }
+            }
         }
-        }
-        }
-        }
-        }
-        }
+    }
+}
+
+
+
+
 
