d81f550bcc32772dc85c291be7f707f7a29cb828
levzzz5154 <86072227+levzzz5154@users.noreply.github.com>
2021-07-20 14:24:47 +0500

    Add files via upload

--- src/minecraft/cat/module/modules/combat/Aura.java
+++ src/minecraft/cat/module/modules/combat/Aura.java
@@ -23,22 +23,36 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 public class Aura extends Module {
+    // values
     public boolean blockStatus = false;
     private final ModeValue mode = new ModeValue("Mode", "Single", true, null, "Single", "Switch", "Multi");
     private final IntegerValue switchDelay = new IntegerValue("Switch Delay", 500, 50, 2000, 50, true, ___ -> mode.get().equals("Switch"));
-    private final ModeValue sortMode = new ModeValue("Sort by", "Health", true,  (a1, a2) -> {target = null; return a2;}, null,"Health", "Distance", "HurtTime");
-    private final IntegerValue minCPS = new IntegerValue("MinCPS", 15, 1, 20, 1, true, (a1, a2) -> {bruh(); return a2;}, null);
-    private final IntegerValue maxCPS = new IntegerValue("MaxCPS", 7, 1, 20, 1, true, (a1, a2) -> {if(minCPS.get() > a2) minCPS.set(a2); return a2;}, null);
+    private final ModeValue sortMode = new ModeValue("Sort by", "Health", true, (a1, a2) -> { target = null; return a2; }, null, "Health", "Distance", "HurtTime");
+
+    private final IntegerValue minCPS = new IntegerValue("MinCPS", 15, 1, 20, 1, true, (a1, a2) -> { bruh(); return a2; }, null);
+    private final IntegerValue maxCPS = new IntegerValue("MaxCPS", 7, 1, 20, 1, true, (a1, a2) -> { if (minCPS.get() > a2) minCPS.set(a2); return a2; }, null);
+
     private final FloatValue range = new FloatValue("Range", 3f, 1f, 6f, 1f, true, null);
-    private final IntegerValue hurtTime = new IntegerValue( "HurtTime", 10, 1, 10, 1, true, __ -> !mode.get().equals("Multi"));
-    private final BooleanValue swing = new BooleanValue( "Swing",true, true, null);
-    private final BooleanValue rotations = new BooleanValue("Rotations",true, true, null);
-    private final BooleanValue silent = new BooleanValue("Silent Rotations",true, true, __ -> rotations.get());
-    private final BooleanValue autoBlock = new BooleanValue( "AutoBlock",true, true, null);
-    private final BooleanValue vanillaAutoBlock = new BooleanValue( "Vanilla Autoblock",true, true, __ -> autoBlock.get());
+    private final IntegerValue hurtTime = new IntegerValue("HurtTime", 10, 1, 10, 1, true, __ -> !mode.get().equals("Multi"));
+    private final BooleanValue swing = new BooleanValue("Swing", true, true, null);
+
+    private final BooleanValue rotations = new BooleanValue("Rotations", true, true, null);
+    private final BooleanValue silent = new BooleanValue("Silent Rotations", true, true, __ -> rotations.get());
+
+    private final FloatValue aimHeight = new FloatValue("Aim Height", 1.5f, 0f, 1.5f, 0.1f, true, __ -> rotations.get());
+
+    private final BooleanValue randomRotations = new BooleanValue("Random Rotations", true, true, __ -> rotations.get());
+    private final FloatValue randomRotVertical = new FloatValue("RRVertical", 0.5f, 0f, 1f, 0.1f, true, __ -> randomRotations.get() && rotations.get());
+    private final FloatValue randomRotHorizontal = new FloatValue("RRHorizontal", 0.5f, 0f, 1f, 0.1f, true, __ -> randomRotations.get() && rotations.get());
+
+    private final BooleanValue autoBlock = new BooleanValue("AutoBlock", true, true, null);
+    private final BooleanValue vanillaAutoBlock = new BooleanValue("Vanilla Autoblock", true, true, __ -> autoBlock.get());
+
+
     public Aura() {
         super("Aura", "", ModuleCategory.COMBAT, Keyboard.KEY_R, "aura", "ka", "killaura");
     }
+
     EntityLivingBase target = null;
     private final MillisTimer timer = new MillisTimer();
     private final MillisTimer switchTimer = new MillisTimer();
@@ -52,7 +66,7 @@ public class Aura extends Module {
                 && mc.thePlayer.getDistanceToEntity(ent) <= range.get())
                 .map(j -> (EntityLivingBase) j) //due to the loadedEntityList being a list of Entity by default, you need to cast every entity to EntityLivingBase
                 .sorted((ent1, ent2) -> {
-                    switch(sortMode.get()) { //this language fucking sucks
+                    switch (sortMode.get()) { //this language fucking sucks
                         case "Distance":
                             return Float.compare(mc.thePlayer.getDistanceToEntity(ent1), mc.thePlayer.getDistanceToEntity(ent2));
 
@@ -62,58 +76,64 @@ public class Aura extends Module {
                         default:
                             return Float.compare(ent1.getHealth(), ent2.getHealth());
                     }
-        }).collect(Collectors.toList());
+                }).collect(Collectors.toList());
 
-        if(list.isEmpty()) {
-            if(blockStatus) {
+        if (list.isEmpty()) {
+            if (blockStatus) {
                 unBlock();
             }
             return;
         }
 
-        if(e.post()) return;
-        switch(mode.get()) {
+        if (e.post()) return;
+        switch (mode.get()) { // la mode
             case "Single":
-                if(target == null || mc.thePlayer.getDistanceToEntity(target) > range.get()) {
+                if (target == null || mc.thePlayer.getDistanceToEntity(target) > range.get()) {
                     target = list.get(0);
                 }
                 attack(target, e);
                 break;
 
             case "Switch":
-                if(target == null || switchTimer.hasTimeReached(switchDelay.get())) {
+                if (target == null || switchTimer.hasTimeReached(switchDelay.get())) {
                     костыль(list);
                     switchTimer.reset();
                 }
-            attack(target, e);
-            break;
+                attack(target, e);
+                break;
 
             case "Multi":
-                if(target == null || target.hurtTime > 0)
+                if (target == null || target.hurtTime > 0) {
                     костыль(list);
+                }
                 attack(target, e);
-            break;
+                break;
         }
     }
 
-    private void костыль(List<EntityLivingBase> f) {
-           int g = f.indexOf(target) + 1;
-           if(g >= f.size()) {
+    private void костыль(List<EntityLivingBase> f) { // чзх
+        int g = f.indexOf(target) + 1;
+        if (g >= f.size()) {
             target = f.get(0);
         } else target = f.get(g);
     }
 
     private void attack(EntityLivingBase target, UpdatePlayerEvent e) {
-        if(target.getHealth() <= 0) { this.target = null; return; }
-        if(target.hurtTime <= hurtTime.get()){
-            if(!vanillaAutoBlock.get()){
+
+        if (target.getHealth() <= 0) {
+            this.target = null;
+            return;
+        }
+
+        if (target.hurtTime <= hurtTime.get()) {
+            if (!vanillaAutoBlock.get()) {
                 unBlock();
             }
-            if(rotations.get()) {
+            if (rotations.get()) {
                 setRotation(e);
             }
-            if(timer.hasTimeReached(ClientUtils.getRandomLong(minCPS.get(), maxCPS.get()))){
-                if(swing.get()) {
+            if (timer.hasTimeReached(ClientUtils.getRandomLong(minCPS.get(), maxCPS.get()))) {
+                if (swing.get()) {
                     mc.thePlayer.swingItem();
                 } else PacketUtil.send(new C0APacketAnimation());
                 mc.getNetHandler().addToSendQueue(new C02PacketUseEntity(target, C02PacketUseEntity.Action.ATTACK));
@@ -122,67 +142,83 @@ public class Aura extends Module {
             block();
         }
     }
-    private void setRotation(UpdatePlayerEvent e){
-        final Vec3 eyesPos = new Vec3(mc.thePlayer.posX, mc.thePlayer.getEntityBoundingBox().minY + mc.thePlayer.getEyeHeight(), mc.thePlayer.posZ);
+
+
+    private void setRotation(UpdatePlayerEvent e) {
+
         AxisAlignedBB bb = target.getEntityBoundingBox();
-        float cd = ClientUtils.getRandomFloat(-0.5f, 0.5f);
-        final Vec3 entPos = new Vec3(bb.minX + (bb.maxX - bb.minX) * (0.5 * cd), bb.minY + (bb.maxY - bb.minY) * (0.5 * cd), bb.minZ + (bb.maxZ - bb.minZ) * (0.5 * cd));
+        // checks if random rots are enabled
+        float randRotVertical = randomRotations.get() ? ClientUtils.getRandomFloat(-randomRotVertical.get(), randomRotVertical.get()) : 0f;
+        float randRotHorizontal = randomRotations.get() ? ClientUtils.getRandomFloat(-randomRotHorizontal.get(), randomRotHorizontal.get()) : 0f;
+
+        final Vec3 eyesPos = new Vec3(mc.thePlayer.posX, mc.thePlayer.getEntityBoundingBox().minY + mc.thePlayer.getEyeHeight(), mc.thePlayer.posZ);
+        final Vec3 entPos = new Vec3(bb.minX + (bb.maxX - bb.minX) * (0.5 * randRotHorizontal), bb.minY + (bb.maxY - bb.minY) * (0.5 * randRotVertical), bb.minZ + (bb.maxZ - bb.minZ) * (0.5 * randRotHorizontal));
+
         final double diffX = entPos.xCoord - eyesPos.xCoord;
-        final double diffY = entPos.yCoord - eyesPos.yCoord;
-        final double diffZ = entPos.zCoord - eyesPos.zCoord;
+        final double diffY = entPos.yCoord - eyesPos.yCoord + aimHeight.get();
+        final double diffZ = entPos.zCoord - eyesPos.zCoord + 0.3; // adding 0.3 unfucks the horizontal rotation
+
         final float yaw = (float) MathHelper.wrapAngleTo180_double(Math.toDegrees(Math.atan2(diffZ, diffX)) - 90F);
-        final float pitch = (float)MathHelper.wrapAngleTo180_double((-Math.toDegrees(Math.atan2(diffY, Math.sqrt(diffX * diffX + diffZ * diffZ + diffY * diffY)))));
-        if(silent.get()){
+        final float pitch = (float) MathHelper.wrapAngleTo180_double((-Math.toDegrees(Math.atan2(diffY, Math.sqrt(diffX * diffX + diffZ * diffZ)))));
+
+        // silent rotations
+        if (silent.get()) {
             e.yaw = yaw;
             e.pitch = pitch;
-        }else{
+        } else {
             mc.thePlayer.rotationYaw = yaw;
             mc.thePlayer.rotationPitch = pitch;
         }
     }
-    public void unBlock(){
+
+    public void unBlock() {
         if (canBlock() || mc.thePlayer.isBlocking()) {
             mc.getNetHandler().addToSendQueue(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
             blockStatus = false;
         }
     }
-    public void block(){
-        if(canBlock() || mc.thePlayer.isBlocking()) {
+
+    public void block() {
+        if (canBlock() || mc.thePlayer.isBlocking()) {
             mc.getNetHandler().addToSendQueue(new C08PacketPlayerBlockPlacement(mc.thePlayer.inventory.getCurrentItem()));
             this.blockStatus = true;
         }
     }
+
     @Override
-    public void onEnable(){
+    public void onEnable() {
         target = null;
         blockStatus = false;
     }
+
     @Override
-    public void onDisable(){
+    public void onDisable() {
         target = null;
-        if(blockStatus){
+        if (blockStatus) {
             unBlock();
             blockStatus = false;
         }
     }
+
     @Subscriber
     public void onPacket(PacketEvent e) {
-        if(e.packet instanceof C09PacketHeldItemChange){
+        if (e.packet instanceof C09PacketHeldItemChange) {
             mc.getNetHandler().getNetworkManager().sendPacketNoEvent(new C07PacketPlayerDigging(C07PacketPlayerDigging.Action.RELEASE_USE_ITEM, BlockPos.ORIGIN, EnumFacing.DOWN));
             blockStatus = false;
         }
     }
-    public boolean canBlock(){
+
+    public boolean canBlock() {
         return mc.thePlayer.getHeldItem() != null && mc.thePlayer.getHeldItem().getItem() instanceof ItemSword && autoBlock.get();
     }
 
     @Override
     public String getTagName() {
-        return this.displayName + " §7" +mode.get();
+        return this.displayName + " §7" + mode.get();
     }
 
     private void bruh() {
-        if(minCPS.get() > maxCPS.get()) {
+        if (minCPS.get() > maxCPS.get()) {
             maxCPS.set(minCPS.get());
         }
     }
