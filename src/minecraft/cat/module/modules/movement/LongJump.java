17bc6aec83bf5163dea3221ad2e33ab7724af757
orangegatogaming <tengosidaasdsi@gmail.com>
2021-08-02 11:17:09 -0600

    Added BypassUtil Added various damage methods Modified LongJump

--- src/minecraft/cat/module/modules/movement/LongJump.java
+++ src/minecraft/cat/module/modules/movement/LongJump.java
@@ -1,63 +1,76 @@
 package cat.module.modules.movement;
 
 import cat.events.Subscriber;
+import cat.events.impl.MoveEvent;
 import cat.events.impl.UpdatePlayerEvent;
 import cat.module.Module;
 import cat.module.ModuleCategory;
 import cat.module.value.types.BooleanValue;
-import cat.ui.notifications.NotiManager;
+import cat.module.value.types.ModeValue;
+import cat.util.BypassUtil;
+import cat.util.MathUtil;
 import cat.util.MovementUtil;
-import net.minecraft.network.play.client.C03PacketPlayer;
+import cat.util.PlayerUtil;
 
 @SuppressWarnings("SpellCheckingInspection")
 public class LongJump extends Module {
+    public ModeValue mode = new ModeValue("Mode", "OldVerus", true, null, "OldVerus");
+    public ModeValue bypassMode = new ModeValue("DamageMode", "Old", false, i -> mode.get().equals("OldVerus"), "None", "Old", "New");
     public LongJump(){
         super("LongJump", "", ModuleCategory.MOVEMENT);
     }
-    private final BooleanValue autoDamage = new BooleanValue("AutoDamage", true, true);
+    public final int[] b = new int[]{0};
+    public final int jumps = 4;
     private int c = 0;
+    private long f = System.currentTimeMillis();
+    private boolean maccacokkk = false; // thx levzzz
     @Override
     public void onEnable(){
         maccacokkk = false;
-        if(this.mc.thePlayer != null && autoDamage.get()){
-            if(mc.theWorld.getCollidingBoundingBoxes(mc.thePlayer, mc.thePlayer.getEntityBoundingBox().offset(0, 3.1, 0).expand(0, 0, 0)).isEmpty()){
-                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX + 1, mc.thePlayer.posY + 3.1, mc.thePlayer.posZ + 1, false));
-                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, false));
-                mc.thePlayer.sendQueue.addToSendQueue(new C03PacketPlayer.C04PacketPlayerPosition(mc.thePlayer.posX, mc.thePlayer.posY, mc.thePlayer.posZ, true));
+        if(this.mc.thePlayer != null && !bypassMode.get().equals("None")){
+            if(bypassMode.get().equals("Old")){
+                PlayerUtil.damageNormal((float) Math.PI);
+                b[0] = -1;
             }else{
-                NotiManager.addNoti("Couldn't damage you: not enough space", "", NotiManager.NotiType.ERROR, 3000);
+                b[0] = 0;
             }
             c = 0;
         }
     }
-    boolean maccacokkk = Boolean.FALSE;
-    float lol = 0;
+    @Subscriber
+    public void onMove(MoveEvent e){
+        if(b[0] <= jumps && bypassMode.get().equals("New")){
+            e.x = 0;
+            e.z = 0;
+        }
+    }
+    public boolean canFly(){
+        return bypassMode.get().equals("None") || bypassMode.get().equals("Old") || (bypassMode.get().equals("New") && b[0] > jumps);
+    }
     @Subscriber
     public void onPlayerUpdate(UpdatePlayerEvent e){
+        if(bypassMode.get().equals("New") && !PlayerUtil.damageVerus(e, jumps, b)){
+            return;
+        }
         if(mc.thePlayer.hurtTime == 9){
             c = 0;
             maccacokkk = true;
         }
-        if(!maccacokkk) return;
-        if(c <= 3){
+        if(!maccacokkk || !canFly()) return;
+        if(c <= 6){
             mc.thePlayer.jump();
             c++;
-            if(c == 3){
+            if(c == 6){
                 f = System.currentTimeMillis();
             }
         }
-        long boostTime = 500;
-        lol = (boostTime - v((System.currentTimeMillis() - f), 0, boostTime)) / boostTime;
+        long boostTime = 700;
+        float lol = (boostTime - MathUtil.inRange((System.currentTimeMillis() - f), 0, boostTime)) / boostTime;
         if(lol > 0.1){
-            MovementUtil.setSpeed(lol / 0.1536f);
+            MovementUtil.setSpeed(lol / BypassUtil.bypass_value);
         }
         if(mc.thePlayer.onGround && lol == 0 && c > 3 && maccacokkk){
             setState(false);
         }
     }
-    public float v(float v, float m, float md){
-        if(v < m) return m;
-        return Math.min(md, v);
-    }
-    long f = System.currentTimeMillis();
 }
